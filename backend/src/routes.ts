897b33feb0a66d4895373b147146c319e820cb03
rodrigoaraujo7 <dev.rodrigoaraujo@gmail.com>
2025-05-17 12:15:03 -0300

    feat: delete route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -7,6 +7,7 @@ import {
 
 import { CreateCustomerController } from "./controllers/CreateCustomerController";
 import { ListCustomersController } from "./controllers/ListCustomersController";
+import { DeleteCustomerController } from "./controllers/DeleteCustomerController";
 
 export async function routes(
   fastify: FastifyInstance,
@@ -32,6 +33,13 @@ export async function routes(
     "/customers",
     async (request: FastifyRequest, reply: FastifyReply) => {
       return new ListCustomersController().handle(request, reply);
+    }
+  );
+
+  fastify.delete(
+    "/customer",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      return new DeleteCustomerController().handle(request, reply);
     }
   );
 }
